0a85ad1bba43c39c4fc64b9f367d08b67c4cb212
Ricardo Lima <rickletras2000@bol.com.br>
2019-11-10 17:30:26 -0300

    projeto iniciado

---
+++ src/main/java/com/ricardo/jobschedule/JobScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.ricardo.jobschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobScheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ricardo/jobschedule/JobScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricardo.jobschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
